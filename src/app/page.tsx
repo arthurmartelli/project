5e8c0b30950b2a3f86ffb88740a06db7accaf51b
Arthur Martelli <arthur.martellig@gmail.com>
2024-02-06 11:16:07 -0600

    feat(db): completed db schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
-import { unstable_noStore as noStore } from "next/cache";
-
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 import ProfileForm from "./clients/page";
 
 export default async function Home() {
-  noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1>{hello.greeting}!</h1>
-      <CrudShowcase />
-    </main>
-  );
-}
-
-async function CrudShowcase() {
-  return (
-    <div className="w-full max-w-xs">
       <ProfileForm />
-    </div>
+    </main>
   );
 }
